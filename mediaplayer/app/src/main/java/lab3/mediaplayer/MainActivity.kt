0b81e9231534dbaaf96770344de4e7d4e95f2b64
hbvhuwe <chernonog.vova@gmail.com>
2019-02-26 09:05:33 +0200

    Add initial project structure for mediaplayer

---
+++ mediaplayer/app/src/main/java/lab3/mediaplayer/MainActivity.kt
@@ -0,0 +1,12 @@
+package lab3.mediaplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
